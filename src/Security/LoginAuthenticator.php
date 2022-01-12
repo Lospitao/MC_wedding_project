df23403546285170e57a0231c7aa4e9bbe1943a8
Laura Lospitao <lauralospitao@gmail.com>
2022-01-12 18:11:58 +0100

    Login #3 - update

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -20,7 +20,7 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
     use TargetPathTrait;
 
     public const LOGIN_ROUTE = 'app_login';
-
+    public const HOME_ROUTE = 'home';
     private UrlGeneratorInterface $urlGenerator;
 
     public function __construct(UrlGeneratorInterface $urlGenerator)
@@ -49,9 +49,7 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate(self::HOME_ROUTE));
     }
 
     protected function getLoginUrl(Request $request): string
